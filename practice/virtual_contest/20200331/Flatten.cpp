3552005ad27c1ae38d83b6695e29c00bf394eff0
hiramekun <thescript1210@gmail.com>
2020-03-31 22:16:49 +0900

    Add virtual_contest 20200331

---
+++ practice/virtual_contest/20200331/Flatten.cpp
@@ -0,0 +1,206 @@
+/**
+ * Created by hiramekun at 08:04 on 2020-03-31.
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template<class T>
+istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+// 素因数分解
+map<ll, ll> prime_factor(ll n) {
+    map<ll, ll> mp;
+    for (ll i = 2; i * i <= n; ++i) {
+        while (n % i == 0) {
+            mp[i]++;
+            n /= i;
+        }
+    }
+    if (n != 1) mp[n]++;
+    return mp;
+}
+
+// 素数判定
+bool is_prime(ll n) {
+    for (int i = 2; i * i <= n; i++) if (n % i == 0) return false;
+    return n != 1;
+}
+
+// 約数列挙
+vector<ll> divisor(ll n) {
+    vector<ll> res;
+    for (ll i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            res.emplace_back(i);
+            if (i != n / i) res.emplace_back(n / i);
+        }
+    }
+    return res;
+}
+
+
+template<std::uint_fast64_t Modulus>
+class modint {
+    using u64 = std::uint_fast64_t;
+
+public:
+    u64 x;
+
+    constexpr modint(const u64 x = 0) noexcept : x(x % Modulus) {}
+
+    constexpr u64 &value() noexcept { return x; }
+
+    constexpr const u64 &value() const noexcept { return x; }
+
+    constexpr modint operator+(const modint rhs) const noexcept {
+        return modint(*this) += rhs;
+    }
+
+    constexpr modint operator-(const modint rhs) const noexcept {
+        return modint(*this) -= rhs;
+    }
+
+    constexpr modint operator*(const modint rhs) const noexcept {
+        return modint(*this) *= rhs;
+    }
+
+    constexpr modint operator/(const modint rhs) const noexcept {
+        return modint(*this) /= rhs;
+    }
+
+    constexpr modint &operator+=(const modint rhs) noexcept {
+        x += rhs.x;
+        if (x >= Modulus) {
+            x -= Modulus;
+        }
+        return *this;
+    }
+
+    constexpr modint &operator-=(const modint rhs) noexcept {
+        if (x < rhs.x) {
+            x += Modulus;
+        }
+        x -= rhs.x;
+        return *this;
+    }
+
+    constexpr modint &operator*=(const modint rhs) noexcept {
+        x = x * rhs.x % Modulus;
+        return *this;
+    }
+
+    constexpr modint &operator/=(modint rhs) noexcept {
+        u64 exp = Modulus - 2;
+        while (exp) {
+            if (exp % 2) {
+                *this *= rhs;
+            }
+            rhs *= rhs;
+            exp /= 2;
+        }
+        return *this;
+    }
+};
+
+typedef vector<modint<mod>> vmod;
+typedef vector<vmod> vvmod;
+
+ll gcd(ll a, ll b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll m, ll n) {
+    if ((0 == m) || (0 == n)) return 0;
+    return ((m / gcd(m, n)) * n);
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2 == 0) {
+        ll t = powmod(x, n / 2, mod);
+        return t * t % mod;
+    }
+    return x * powmod(x, n - 1, mod) % mod;
+}
+
+
+void solve() {
+    ll n;
+    cin >> n;
+    vl a(n);
+    cin >> a;
+
+    ump<ll, ll> mp;
+    rep(i, n) {
+        auto tmp = prime_factor(a[i]);
+        each(e, tmp) {
+            mp[e.F] = max(mp[e.F], e.S);
+        }
+    }
+
+    modint<mod> lcm = 1;
+    each(e, mp) {
+        lcm *= powmod(e.F, e.S, mod);
+    }
+    modint<mod> ans = 0;
+    rep(i, n) {
+        ans += lcm / a[i];
+    }
+    cout << ans.x << '\n';
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
