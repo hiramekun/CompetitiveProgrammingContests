a454fcf66ee82437dea6f0132dd82bd1b0df9230
hiramekun <thescript1210@gmail.com>
2020-04-04 20:37:34 +0900

    Add abc160

---
+++ contest/abc/abc160/e.cpp
@@ -0,0 +1,86 @@
+/**
+ * Created by hiramekun at 18:48 on 2020-04-04.
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template<class T>
+istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
+void solve() {
+    ll x, y, a, b, c;
+    cin >> x >> y >> a >> b >> c;
+
+    vl p(a), q(b), r(c);
+    cin >> p >> q >> r;
+    sort(all(p)), reverse(all(p));
+    sort(all(q)), reverse(all(q));
+    sort(all(r)), reverse(all(r));
+    vl px, qy;
+    rep(i, x) px.eb(p[i]);
+    rep(i, y) qy.eb(q[i]);
+
+    vl pqr = px;
+    rep(i, y) pqr.eb(qy[i]);
+    rep(i, c) pqr.eb(r[i]);
+    sort(all(pqr)), reverse(all(pqr));
+    ll ans = 0;
+    rep(i, x + y) ans += pqr[i];
+
+    cout << ans << '\n';
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
