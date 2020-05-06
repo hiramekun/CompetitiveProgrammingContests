5f0bdfb4c496cb91ae39ef3cf92b010b92778c6b
hiramekun <thescript1210@gmail.com>
2020-05-07 02:03:28 +0900

    Add abc158

---
+++ contest/abc/abc158/d.cpp
@@ -0,0 +1,93 @@
+/**
+ * Created by hiramekun at 01:55 on 2020-05-07.
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template<class T>
+istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
+void solve() {
+    string s;
+    cin >> s;
+    ll q;
+    cin >> q;
+
+    deque<char> que(all(s));
+    bool rev = false;
+    while (q--) {
+        ll t;
+        cin >> t;
+        if (t == 1) {
+            rev = !rev;
+        } else {
+            ll f;
+            char c;
+            cin >> f >> c;
+            if (rev) f = 3 - f;
+            if (f == 1) {
+                que.push_front(c);
+            } else {
+                que.push_back(c);
+            }
+        }
+    }
+    string ans = string(all(que));
+    if (rev) reverse(all(ans));
+    cout << ans << '\n';
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
