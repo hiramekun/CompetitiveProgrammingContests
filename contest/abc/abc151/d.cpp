bbe60dbf4514625ebe290605bba024b6e8357c20
hiramekun <thescript1210@gmail.com>
2020-05-12 09:04:11 +0900

    Add abc151

---
+++ contest/abc/abc151/d.cpp
@@ -0,0 +1,102 @@
+/**
+ * Created by hiramekun at 01:18 on 2020-05-12.
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template<class T>
+istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
+
+void solve() {
+    ll h, w;
+    cin >> h >> w;
+    vector<string> s(h);
+    rep(i, h) {
+        cin >> s[i];
+    }
+
+    ll ans = 0;
+    rep(i, h) {
+        rep(j, w) {
+            if (s[i][j] == '#') continue;
+
+            vector<vector<ll>> d(h, vector<ll>(w, ll_inf));
+            queue<P> q;
+            q.push(make_pair(i, j));
+            d[i][j] = 0;
+            ll tmp = 0;
+            while (!q.empty()) {
+                ll x = q.front().F, y = q.front().S;
+                q.pop();
+                rep(k, 4) {
+                    ll nx = x + dx[k], ny = y + dy[k];
+                    if (nx < 0 || h - 1 < nx || ny < 0 || w - 1 < ny) continue;
+                    if (d[nx][ny] < ll_inf || s[nx][ny] == '#') continue;
+
+                    d[nx][ny] = d[x][y] + 1;
+                    tmp = max(tmp, d[nx][ny]);
+                    q.push(make_pair(nx, ny));
+                }
+            }
+            ans = max(ans, tmp);
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
---
+++ contest/abc/abc151/f.cpp
@@ -0,0 +1,86 @@
+/**
+ * Created by hiramekun at 00:19 on 2020-01-13.
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template<class T>
+istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
+double mydist(double x1, double x2, double y1, double y2) {
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+
+void solve() {
+    ll n;
+    cin >> n;
+    vl x(n), y(n);
+    rep(i, n) cin >> x[i] >> y[i];
+
+    double ans = double(ll_inf);
+    rep(xi, 1001) {
+        rep(yi, 1001) {
+            double tmp = 0;
+            rep(i, n) {
+                tmp = max(tmp, mydist(xi, x[i], yi, y[i]));
+            }
+            ans = min(ans, tmp);
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
