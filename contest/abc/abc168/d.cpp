df06a3e136a3834ba3f83867332c75664b9a1328
hiramekun <thescript1210@gmail.com>
2020-05-18 10:03:49 +0900

    Add 168

---
+++ contest/abc/abc168/d.cpp
@@ -0,0 +1,96 @@
+/**
+ * Created by hiramekun at 20:56 on 2020-05-17.
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template<class T>
+istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
+void solve() {
+    ll n, m;
+    cin >> n >> m;
+    vector<vector<ll>> g(n);
+    rep(i, m) {
+        ll a, b;
+        cin >> a >> b;
+        a--, b--;
+        g[a].eb(b), g[b].eb(a);
+    }
+    vector<bool> seen(n);
+    vector<ll> ans(n);
+    auto rec = [&](auto &&f, vl &nxt) -> void {
+        vector<ll> next;
+        each(nx, nxt) {
+            each(e, g[nx]) {
+                if (seen[e]) continue;
+                seen[e] = true;
+                ans[e] = nx + 1;
+                next.eb(e);
+            }
+        }
+        if (next.empty()) return;
+        f(f, next);
+    };
+    vl tmp = vector<ll>{0};
+    rec(rec, tmp);
+    cout << "Yes" << '\n';
+    for (ll i = 1; i < n; ++i) {
+        cout << ans[i] << '\n';
+    }
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
