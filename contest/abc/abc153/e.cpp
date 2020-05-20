857c14c3c7ada998cc2e3977f68f40bf1710ce29
hiramekun <thescript1210@gmail.com>
2020-05-20 23:05:05 +0900

    Add e

---
+++ contest/abc/abc153/e.cpp
@@ -0,0 +1,87 @@
+/**
+ * Created by hiramekun at 22:15 on 2020-05-20.
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template<class T>
+istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
+void solve() {
+    ll h, n;
+    cin >> h >> n;
+
+    vl a(n), b(n);
+    rep(i, n) {
+        cin >> a[i] >> b[i];
+    }
+    vl dp(2 * 1e4 + 1, ll_inf); // dp[i]: 体力をi減らすため消耗する魔力の最小値
+    dp[0] = 0;
+    rep(i, 2 * 1e4 + 1) {
+        rep(j, n) {
+            if (i < a[j]) continue;
+            dp[i] = min(dp[i], dp[i - a[j]] + b[j]);
+        }
+    }
+    ll ans = ll_inf;
+    for (ll r = h; r <= 2 * 1e4; ++r) {
+        ans = min(ans, dp[r]);
+
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
