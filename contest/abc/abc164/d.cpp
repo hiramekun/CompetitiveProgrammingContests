d4b9a7bf256a7c0bfb4311ca7aefefec5d9caa51
hiramekun <thescript1210@gmail.com>
2020-05-25 02:32:44 +0900

    Update d

--- contest/abc/abc164/d.cpp
+++ contest/abc/abc164/d.cpp
@@ -1,21 +1,63 @@
-/**
- * Created by hiramekun at 20:53 on 2020-04-26.
- */
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
-#define each(i, mp) for(auto& i:mp)
 
-int main() {
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 2019;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template<class T>
+istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
+void solve() {
     string s;
     cin >> s;
-    map<ll, ll> mp;
-    mp[0] = 1;
-    reverse(s.begin(), s.end());
-    ll now = 0, ten = 1;
-    each(e, s) {
-        now = (now + (e - '0') * ten) % 2019;
+    reverse(all(s));
+    ll n = s.size();
+    ll ten = 1;
+    ll now = 0;
+    unordered_map<ll, ll> mp{make_pair(0, 1)};
+    rep(i, n) {
+        now = (now + (s[i] - '0') * ten) % 2019;
         ten *= 10;
         ten %= 2019;
         mp[now]++;
@@ -25,5 +67,15 @@ int main() {
         ans += e.second * (e.second - 1) / 2;
     }
     cout << ans << '\n';
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
     return 0;
 }
