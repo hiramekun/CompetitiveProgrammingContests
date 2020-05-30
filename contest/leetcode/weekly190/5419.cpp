abf70d7e26827213646233c9364e28cc0ba80795
hiramekun <thescript1210@gmail.com>
2020-05-30 20:34:38 +0900

    Update

--- contest/leetcode/weekly190/5419.cpp
+++ contest/leetcode/weekly190/5419.cpp
@@ -1,10 +1,5 @@
-/**
- * Created by hiramekun at 12:08 on 2020-05-24.
- */
 #ifdef MY_DEBUG
-
 #include <bits/stdc++.h>
-
 using namespace std;
 #endif
 
@@ -13,12 +8,9 @@ using vl = vector<ll>;
 using vvl = vector<vl>;
 using vb = vector<bool>;
 using P = pair<ll, ll>;
-template<typename T>
-using pq = priority_queue<T>;
-template<typename T>
-using minpq = priority_queue<T, vector<T>, greater<T>>;
-template<typename T, typename K>
-using ump = unordered_map<T, K>;
+template <typename T> using pq = priority_queue<T>;
+template <typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template <typename T, typename K> using ump = unordered_map<T, K>;
 const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 const ll mod = 1000000007;
 const ll inf = ll(1e9);
@@ -33,49 +25,39 @@ const ll ll_inf = ll(1e9) * ll(1e9);
 #define S second
 #define all(obj) (obj).begin(), (obj).end()
 
-
-/* ------------- ANSWER ------------- */
-/* ---------------------------------- */
-template<class T>
-ostream &operator<<(ostream &out, const vector<T> &list) {
+template <class T> ostream &operator<<(ostream &out, const vector<T> &list) {
     ll n = list.size();
     rep(i, n) out << list[i] << ' ';
     return out;
 }
 
-template<class T>
-istream &operator>>(istream &in, vector<T> &list) {
+template <class T> istream &operator>>(istream &in, vector<T> &list) {
     ll n = list.size();
     rep(i, n) in >> list[i];
     return in;
 }
 
-template<class T>
-ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+template <class T> ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
     ll n = list.size();
     rep(i, n) out << list[i] << '\n';
     return out;
 }
 
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
 class Solution {
 public:
     int maxDotProduct(vector<int> &nums1, vector<int> &nums2) {
-        int n1 = nums1.size(), n2 = nums2.size();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, INT_MIN));
-        dp[0][0] = 0;
-        for (int i1 = 1; i1 <= n1; ++i1) {
-            for (int i2 = 1; i2 <= n2; ++i2) {
-                dp[i1][i2] = max(dp[i1][i2 - 1], dp[i1][i2]);
-                dp[i1][i2] = max(dp[i1 - 1][i2], dp[i1][i2]);
-                dp[i1][i2] = max(dp[i1][i2],
-                                 max(0, dp[i1 - 1][i2 - 1]) + nums1[i1 - 1] * nums2[i2 - 1]);
+        ll n1 = nums1.size(), n2 = nums2.size();
+        vvl dp(n1 + 1, vl(n2 + 1, INT_MIN));
+        for (ll i = 1; i <= n1; ++i) {
+            for (ll j = 1; j <= n2; ++j) {
+                dp[i][j] = max(dp[i][j], dp[i][j - 1]);
+                dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+                dp[i][j] = max(dp[i][j], max(dp[i - 1][j - 1], 0LL) + nums1[i - 1] * nums2[j - 1]);
             }
         }
-        return dp[n1][n2];
+        return dp.back().back();
     }
 };
-
-int main() {
-    vector<int> nums1{2, 1, -2, 5}, nums2{3, 0, -6};
-    cout << Solution().maxDotProduct(nums1, nums2) << '\n';
-}
