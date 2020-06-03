d973b5b32adab3ec2bcc347de2e3c932f1fb88ec
hiramekun <thescript1210@gmail.com>
2020-06-04 01:07:52 +0900

    Add abc155

---
+++ contest/abc/abc155/C/main.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+template <typename T> using pq = priority_queue<T>;
+template <typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template <typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for (ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for (auto &i : mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template <class T> ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template <class T> istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template <class T> ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
+void solve() {
+    ll n;
+    cin >> n;
+    map<string, ll> mp;
+    rep(i, n) {
+        string s;
+        cin >> s;
+        mp[s]++;
+    }
+    vector<pair<string, ll>> v(mp.begin(), mp.end());
+    // sort from bigger.
+    sort(v.begin(), v.end(), [](pair<string, ll> a, pair<string, ll> b) {
+        if (a.second == b.second) {
+            return a.first < b.first;
+        }
+        return a.second > b.second;
+    });
+    cout << v[0].F << '\n';
+    for (ll i = 0; i < v.size() - 1; ++i) {
+        if (v[i].S == v[i + 1].S) {
+            cout << v[i + 1].F << '\n';
+        } else {
+            break;
+        }
+    }
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
