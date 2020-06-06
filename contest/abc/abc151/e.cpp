0014f79cdcd2a0ba3a989a6f70e09dfd955f22c4
hiramekun <thescript1210@gmail.com>
2020-06-06 22:09:42 +0900

    Add e

---
+++ contest/abc/abc151/e.cpp
@@ -0,0 +1,203 @@
+/**
+ * Created by hiramekun at 21:15 on 2020-06-06.
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template<class T>
+istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+template<std::uint_fast64_t Modulus>
+class modint {
+    using u64 = std::uint_fast64_t;
+
+public:
+    u64 x;
+
+    constexpr modint(const u64 x = 0) noexcept : x(x % Modulus) {}
+
+    constexpr u64 &value() noexcept { return x; }
+
+    constexpr const u64 &value() const noexcept { return x; }
+
+    constexpr modint operator+(const modint rhs) const noexcept {
+        return modint(*this) += rhs;
+    }
+
+    constexpr modint operator-(const modint rhs) const noexcept {
+        return modint(*this) -= rhs;
+    }
+
+    constexpr modint operator*(const modint rhs) const noexcept {
+        return modint(*this) *= rhs;
+    }
+
+    constexpr modint operator/(const modint rhs) const noexcept {
+        return modint(*this) /= rhs;
+    }
+
+    constexpr modint &operator+=(const modint rhs) noexcept {
+        x += rhs.x;
+        if (x >= Modulus) {
+            x -= Modulus;
+        }
+        return *this;
+    }
+
+    constexpr modint &operator-=(const modint rhs) noexcept {
+        if (x < rhs.x) {
+            x += Modulus;
+        }
+        x -= rhs.x;
+        return *this;
+    }
+
+    constexpr modint &operator*=(const modint rhs) noexcept {
+        x = x * rhs.x % Modulus;
+        return *this;
+    }
+
+    constexpr modint &operator/=(modint rhs) noexcept {
+        u64 exp = Modulus - 2;
+        while (exp) {
+            if (exp % 2) {
+                *this *= rhs;
+            }
+            rhs *= rhs;
+            exp /= 2;
+        }
+        return *this;
+    }
+};
+
+
+ostream &operator<<(ostream &out, modint<mod> rhs) {
+    out << rhs.x << endl;
+    return out;
+}
+
+typedef vector<modint<mod>> vmod;
+typedef vector<vmod> vvmod;
+const int MAX = 510000;
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++) {
+        fac[i] = fac[i - 1] * i % mod;
+        inv[i] = mod - inv[mod % i] * (mod / i) % mod;
+        finv[i] = finv[i - 1] * inv[i] % mod;
+    }
+}
+
+// 二項係数計算
+ll COM(int n, int k) {
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % mod) % mod;
+}
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+    vl a(n);
+    cin >> a;
+    sort(all(a));
+    ump<ll, ll> mp;
+    rep(i, n) mp[a[i]]++;
+    vl ijyo(n), ika(n);
+    ll cnt = 0;
+
+    repr(i, n) {
+        ijyo[i] = mp[a[i]] + cnt - 1;
+        ll cont = 1;
+        while (i - 1 >= 0 && a[i] == a[i - 1]) {
+            i--;
+            ijyo[i] = mp[a[i]] + cnt - 1 - cont;
+            cont++;
+        }
+        cnt += mp[a[i]];
+    }
+    cnt = 0;
+    rep(i, n) {
+        ika[i] = mp[a[i]] + cnt - 1;
+        ll cont = 1;
+        while (i + 1 <= n - 1 && a[i] == a[i + 1]) {
+            i++;
+            ika[i] = mp[a[i]] + cnt - 1 - cont;
+            cont++;
+        }
+        cnt += mp[a[i]];
+    }
+
+    ll minus = 0, plus = 0;
+    COMinit();
+    rep(i, n) {
+        minus += (COM(ijyo[i], k - 1) * a[i]) % mod;
+        minus %= mod;
+        plus += (COM(ika[i], k - 1) * a[i]) % mod;
+        plus %= mod;
+    }
+    if (plus - minus < 0) {
+        plus += mod;
+    }
+    cout << (plus - minus) % mod << '\n';
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
