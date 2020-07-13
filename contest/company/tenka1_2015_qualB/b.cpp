1f09c28840ce58a92acb844627ac9d9ec6156669
hiramekun <thescript1210@gmail.com>
2020-07-14 08:58:23 +0900

    Add tenka1_2015_qualB

---
+++ contest/company/tenka1_2015_qualB/b.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+template <typename T> using pq = priority_queue<T>;
+template <typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template <typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for (ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for (auto &i : mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template <class T> ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template <class T> istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template <class T> ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
+void solve() {
+    string s;
+    cin >> s;
+    ll n = s.size();
+    bool has_commma = false, has_colon = false;
+    queue<char> prams;
+    rep(i, n) {
+        if (s[i] == '{') prams.push('{');
+        else if (s[i] == '}') prams.pop();
+        else if (prams.size() == 1 && s[i] == ':') has_colon = true;
+        else if (prams.size() == 1 && s[i] == ',') has_commma = true;
+    }
+    if (s == "{}") {
+        cout << "dict" << '\n';
+    } else {
+        if (has_colon) {
+            cout << "dict" << '\n';
+        } else {
+            cout << "set" << '\n';
+        }
+    }
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
