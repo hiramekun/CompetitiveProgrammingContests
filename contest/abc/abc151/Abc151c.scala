960639a2383cbe222d65129c0adb3615d2169664
hiramekun <thescript1210@gmail.com>
2020-10-25 21:13:20 +0900

    Add ABC151c

---
+++ contest/abc/abc151/Abc151c.scala
@@ -0,0 +1,23 @@
+object Main extends App {
+  val pw = new java.io.PrintWriter(System.out)
+  val sc = new java.util.Scanner(System.in)
+
+  val n, m = sc.nextInt
+  val aced = new Array[Boolean](n + 1)
+  val penalty = new Array[Int](n + 1)
+
+  (1 to m).foreach { _ =>
+    val p = sc.nextInt
+    val s = sc.next
+    if (s == "AC") aced(p) = true
+    else if (!aced(p)) penalty(p) = penalty(p) + 1
+  }
+  val (ac, wa) = aced.zip(penalty).foldRight((0, 0)) {
+    case ((aced, penalty), (accAc, accPen)) =>
+      if (aced) (accAc + 1, accPen + penalty)
+      else (accAc, accPen)
+  }
+
+  pw.println(s"$ac $wa")
+  pw.flush()
+}
